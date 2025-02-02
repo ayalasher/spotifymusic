e1d24a363be893511e55590990985474303e90f5
ayalasher <144620362+ayalasher@users.noreply.github.com>
2025-02-02 12:18:03 +0300

    screens added

---
+++ screens/Createscreen.js
@@ -0,0 +1,11 @@
+import { Text, View , StyleSheet } from "react-native";
+
+export default function Createscreen() {
+    return <View>
+        <Text>Create items screen</Text>
+    </View>
+}
+
+const styles = StyleSheet.create({
+    
+})
