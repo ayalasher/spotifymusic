52308bcc7680087bc4cc03b95a493fa37c673ddd
ayalasher <144620362+ayalasher@users.noreply.github.com>
2025-02-06 10:38:10 +0300

    Working on the search page

--- screens/Searchscreen.js
+++ screens/Searchscreen.js
@@ -1,10 +1,27 @@
-import { View , Text , StyleSheet } from "react-native";
+import { View , Text , StyleSheet, Pressable } from "react-native";
+import Hometopbuttonns from "../buttons/Hometopbuttons";
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function Search() {
     return <View style={styles.container} >
-        <Text>
-            Search screen
-        </Text>
+        <View style={styles.topsectioncontainer} >
+
+                <View style={styles.profileandsearch} >
+                    <View style={styles.btncontainer}>
+                     <Hometopbuttonns>P</Hometopbuttonns>
+                    </View>
+                    <Text style={styles.topsectiontxt} >Search</Text>
+                </View>
+
+                <View style={styles.cameraiconcontainer}  >
+                    <Pressable>
+                       <AntDesign name="camerao" size={27} color="black" />
+                    </Pressable>
+                </View>
+                
+        </View>
+       
+
     </View>
 }
 
@@ -13,6 +30,35 @@ const styles = StyleSheet.create({
         flex:1,
         paddingVertical:50,
         paddingHorizontal:20,
+        backgroundColor:"#353d37"
 
-    }
+    },
+    topsectioncontainer:{
+    //    backgroundColor:"red",
+       flexDirection:"row",
+       justifyContent:"space-between"
+    },
+    profileandsearch:{
+       flexDirection:"row",
+       alignItems:"center", 
+       justifyContent:"center",
+       paddingVertical:15
+    },
+
+    cameraiconcontainer:{
+       flexDirection:"row",
+       alignItems:"center", 
+       justifyContent:"center"
+    },
+    topsectiontxt:{
+        paddingHorizontal:10,
+        fontSize:25,
+        fontWeight:"900",
+    } ,
+    profilebutton:{
+       
+    },
+    btncontainer:{
+    
+    },
 })
