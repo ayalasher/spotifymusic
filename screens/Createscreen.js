d8b4401d3354a1522bb3cffd519d5316878f7152
ayalasher <144620362+ayalasher@users.noreply.github.com>
2025-02-26 21:28:46 +0300

    fixing the create screen

--- screens/Createscreen.js
+++ screens/Createscreen.js
@@ -3,7 +3,15 @@ import { Text, View, StyleSheet } from "react-native";
 export default function Createscreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.pagetxt}>Create items screen</Text>
+      <Text style={styles.pagetxt}>
+        Get creative with your personal playlist combine tastes in a shared
+        playlist with friends{" "}
+      </Text>
+      <View
+        style={{
+          flexDirection: "column",
+        }}
+      ></View> 
     </View>
   );
 }
@@ -12,7 +20,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingVertical: 50,
-    paddingHorizontal: 20,
+    paddingHorizontal: 35,
     backgroundColor: "#121212",
   },
   pagetxt: {
